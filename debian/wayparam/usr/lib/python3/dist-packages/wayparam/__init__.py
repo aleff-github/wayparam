59f7fc25be85bcbe93dce330751dcb5a18c9539c
Aleff <alessandro.greco.1@protonmail.com>
2025-12-28 15:41:25 +0100

    Remove Debian packaging directory from upstream repository

--- debian/wayparam/usr/lib/python3/dist-packages/wayparam/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0
-
-__all__ = ["__version__"]
-__version__ = "0.3.0"
--- debian/wayparam/usr/lib/python3/dist-packages/wayparam/__main__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0
-
-from .cli import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
