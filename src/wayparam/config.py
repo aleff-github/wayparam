9d831e84b67dd13e5bd541c806f30676e75e3fde
Aleff <alessandro.greco.1@protonmail.com>
2026-09-21 16:33:30 +0200

    Apply ruff formatting to analysis mode declaration

--- src/wayparam/config.py
+++ src/wayparam/config.py
@@ -20,7 +20,9 @@ from .output import OutputFormat
 from .providers import CommonCrawlOptions, SourceName
 from .wayback import CdxOptions
 
-AnalysisMode = Literal["history", "params", "summary", "timeline", "changes", "topology", "cooccurrence"]
+AnalysisMode = Literal[
+    "history", "params", "summary", "timeline", "changes", "topology", "cooccurrence"
+]
 TimelineGranularity = Literal["year", "month"]
 
 
